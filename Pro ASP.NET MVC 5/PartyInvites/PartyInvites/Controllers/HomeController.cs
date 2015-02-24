a912d4d9dffeb2e972923d5527f4a6d2194c860d
Joe Kucera <joekucera2002@gmail.com>
2015-02-24 12:36:28 -0700

    Implemented basic functionality.

--- Pro ASP.NET MVC 5/PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ Pro ASP.NET MVC 5/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PartyInvites.Models;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
         // GET: Home
-        public ActionResult Index()
+        public ViewResult Index()
+        {
+            Int32 hour = DateTime.Now.Hour;
+
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+
+            return View();
+        }
+
+        // GET: Home/RsvpForm
+        [HttpGet]
+        public ViewResult RsvpForm()
         {
             return View();
+        }
+
+        // POST: Home/RsvpForm
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            return View("Thanks", guestResponse);
         }
     }
 }
