ffb6822c988d4015887683b81f6b28245f808e20
Joe Kucera <joekucera2002@gmail.com>
2015-03-03 10:23:19 -0700

    Added object/collection initializers example.

--- Pro ASP.NET MVC 5/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ Pro ASP.NET MVC 5/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -29,7 +29,40 @@ namespace LanguageFeatures.Controllers
             String productName = myProduct.Name;
 
             // Generate the view
-            return View("Result", (object)String.Format("ProductName: {0}", productName));
+            return View("Result", (Object)String.Format("ProductName: {0}", productName));
+        }
+
+        // GET: Home/CreateProduct
+        public ViewResult CreateProduct()
+        {
+            // Create and populate a new Product object
+            Product myProduct = new Product
+            {
+                ProductId = 100,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M,
+                Category = "Watersports"
+            };
+
+            return View("Result", (Object)String.Format("Category: {0}", myProduct.Category));
+        }
+
+        // GET: Home/CreateCollection
+        public ViewResult CreateCollection()
+        {
+            String[] stringArray = {"apple", "orange", "plum"};
+
+            List<Int32> intList = new List<Int32> {10, 20, 30, 40};
+
+            Dictionary<String, Int32> myDict = new Dictionary<string, Int32>
+            {
+                {"apple", 10},
+                {"orange", 20},
+                {"plum", 30}
+            };
+
+            return View("Result", (Object)stringArray[1]);
         }
     }
 }
