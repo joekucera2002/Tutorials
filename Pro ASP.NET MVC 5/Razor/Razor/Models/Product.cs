38baba997a4f3f9c63a01c951d652963e6bac287
Joe Kucera <joekucera2002@gmail.com>
2015-03-05 09:12:30 -0700

    Added initial controller/view.

---
+++ Pro ASP.NET MVC 5/Razor/Razor/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Razor.Models
+{
+    public class Product
+    {
+        public Int32 ProductId { get; set; }
+        public String Name { get; set; }
+        public String Description { get; set; }
+        public Decimal Price { get; set; }
+        public String Category { get; set; }
+    }
+}
